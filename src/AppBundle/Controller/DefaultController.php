e99a868217cb2a60f446a04c9e5a829d0f16f781
Rocco Bruyn <rocco.bruijn@clockwork.nl>
2016-01-04 13:16:08 +0100

    Render list of bladen
    Get DAO from contailer and pass it to the view. Use the DAO to fetch
    list of bladen and render it into a list.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,12 +10,15 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param  Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $dao = $this->get('app.dao.kb');
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'dao' => $dao,
         ]);
     }
 }
