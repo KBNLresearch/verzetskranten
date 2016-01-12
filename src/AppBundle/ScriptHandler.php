a5f5496f04e9fe3ad9eec1d8153df5cea18c2569
Rocco Bruyn <rocco.bruijn@clockwork.nl>
2016-01-12 14:10:35 +0100

    Use DIRECTORY_SEPARATOR constant in script handler

--- src/AppBundle/ScriptHandler.php
+++ src/AppBundle/ScriptHandler.php
@@ -8,14 +8,14 @@ class ScriptHandler
 {
     public static function createCacheDirs(Event $evt)
     {
-        $base = 'var/cache/';
+        $base = 'var' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR;
         $envs = ['dev', 'test', 'prod'];
 
         foreach ($envs as $env) {
             $path = realpath($base . $env);
 
             if (file_exists($path) && is_dir($path)) {
-                mkdir($path . '/app', 0760, true);
+                mkdir($path . DIRECTORY_SEPARATOR . 'app', 0760, true);
             }
         }
     }
